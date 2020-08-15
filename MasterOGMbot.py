ea25c42dab4288ce8fce794399ccfc7370dffb94
Toizzystradlin <Toizzystradlin@yandex.ru>
2020-08-15 13:57:47 +0300

    сделал удаление кнопочек с сотрудниками

--- MasterOGMbot.py
+++ MasterOGMbot.py
@@ -11,7 +11,7 @@ from datetime import datetime
 while True:
     try:
 
-
+        n_emp = 0
         db = mysql.connector.connect(
             host='localhost',
             user='root',
@@ -79,67 +79,57 @@ while True:
                                 bot.send_message(call.message.chat.id, '*id: *' + str(emp[0]), reply_markup=keyboard, parse_mode="Markdown")
                         else:
                             bot.send_message(call.message.chat.id, 'На эту заявку уже назначены сотрудники')
+                    keyboard = telebot.types.InlineKeyboardMarkup()
+
+                    bot.edit_message_reply_markup(chat_id=call.message.chat.id,
+                                                  message_id=call.message.message_id,
+                                                  reply_markup=keyboard)
                 except:
                     print('ошибка в чуз')
 
             elif call.data == 'choose_man':
-                try:
-                    db = mysql.connector.connect(
-                        host='localhost',
-                        user='root',
-                        passwd='12345',
-                        port='3306',
-                        database='ogm2'
-                    )
-                    cursor = db.cursor()
-
-                    emp_id = call.message.chat.id  # блок выделения id сотрудника
-                    sql = "SELECT employee_id FROM employees WHERE (tg_id = %s)"
-                    val = (emp_id,)
-                    cursor.execute(sql, val)
-                    master_id = cursor.fetchone()[0]
-
-
-                    ids = re.findall(r'\d+', call.message.text)  # блок считывания id сотрудника
-                    MQuery['employee_id'] = ids[0]
-                    print(MQuery['employee_id'])
-
-                    sql = "SELECT fio, tg_id FROM employees WHERE employee_id = %s"
-                    val = (MQuery['employee_id'],)
-                    cursor.execute(sql, val)
-                    fio = cursor.fetchone()
-
-                    doers = []
-                    doers.append(MQuery['employee_id'])
-                    doers_dict = {'doers': doers}
-                    doers_json = json.dumps(doers_dict)
-
-                    sql = "UPDATE queries SET json_emp = %s, appointer = %s, query_status = %s WHERE query_id = %s"  # назначение сотрудника
-                    val = (doers_json, master_id, 'Принята', MQuery['query_id'])
-                    cursor.execute(sql, val)
-                    db.commit()
+                #try:
+                db = mysql.connector.connect(
+                    host='localhost',
+                    user='root',
+                    passwd='12345',
+                    port='3306',
+                    database='ogm2'
+                )
+                cursor = db.cursor()
+                emp_id = call.message.chat.id  # блок выделения id сотрудника
+                sql = "SELECT employee_id FROM employees WHERE (tg_id = %s)"
+                val = (emp_id,)
+                cursor.execute(sql, val)
+                master_id = cursor.fetchone()[0]
+                ids = re.findall(r'\d+', call.message.text)  # блок считывания id сотрудника
+                MQuery['employee_id'] = ids[0]
+                print(MQuery['employee_id'])
+                sql = "SELECT fio, tg_id FROM employees WHERE employee_id = %s"
+                val = (MQuery['employee_id'],)
+                cursor.execute(sql, val)
+                fio = cursor.fetchone()
+                doers = []
+                doers.append(MQuery['employee_id'])
+                doers_dict = {'doers': doers}
+                doers_json = json.dumps(doers_dict)
+                sql = "UPDATE queries SET json_emp = %s, appointer = %s, query_status = %s WHERE query_id = %s"  # назначение сотрудника
+                #val = (doers_json, master_id, 'Принята', MQuery['query_id'])
+                val = (doers_json, master_id, 'Принята', MQuery['query_id'])
+                cursor.execute(sql, val)
+                db.commit()
 
 
 
-                    keyboard = telebot.types.InlineKeyboardMarkup()
-                    key_1 = telebot.types.InlineKeyboardButton("Да", callback_data='choose_more')
-                    keyboard.add(key_1)
-                    key_2 = telebot.types.InlineKeyboardButton("Нет", callback_data='sent')
-                    keyboard.add(key_2)
-                    bot.send_message(call.message.chat.id, fio[0] + ' назначен, назначить еще кого-то?', reply_markup=keyboard)
-
-                    #sql = "SELECT fio, tg_id FROM employees WHERE (employee_id = %s) LIMIT 0, 1"
-                    #val = (MQuery['employee_id'],)
-                    #cursor.execute(sql, val)
-                    #man = cursor.fetchone()
-        #
-                    #Send_message.send_message_3(MQuery['query_id'])  # выводит мастеру инфо о заявке
-                    #bot.send_message(call.message.chat.id, 'заявка отправлена исполнителю: ' + man[0])
-                    #bot.delete_message(call.message.chat.id, message_id=call.message.message_id - 1)
-                    Send_message.send_message_2(fio[1], MQuery['query_id'])  # отправка исполнителю
-                    #bot.delete_message(call.message.chat.id, message_id=call.message.message_id)
-                except:
-                    print('ошибка в чуз мэн')
+                keyboard = telebot.types.InlineKeyboardMarkup()
+                key_1 = telebot.types.InlineKeyboardButton("Да", callback_data='choose_more')
+                keyboard.add(key_1)
+                key_2 = telebot.types.InlineKeyboardButton("Нет", callback_data='sent')
+                keyboard.add(key_2)
+                bot.send_message(call.message.chat.id, fio[0] + ' назначен, назначить еще кого-то?', reply_markup=keyboard)
+                Send_message.send_message_2(fio[1], MQuery['query_id'])  # отправка исполнителю
+                #except:
+                #    print('ошибка в чуз мэн')
 
             elif call.data == 'choose_more':
                 db = mysql.connector.connect(
@@ -162,6 +152,14 @@ while True:
                 json_emp_list = json_emp_dict['doers']
                 print(type(json_emp_list[0]))
 
+                sql = "SELECT * FROM employees"
+                cursor.execute(sql, )
+                empsn = cursor.fetchall()
+                n = 0
+                for emp in empsn:
+                    n += 1
+                    bot.delete_message(call.message.chat.id, call.message.message_id - n)
+
                 for i in emps:
                     print(type(i[0]))
                     if i[0] != int(json_emp_list[0]):
@@ -171,6 +169,7 @@ while True:
                         bot.send_message(call.message.chat.id,
                                          '*id: *' + str(i[0]),
                                          reply_markup=keyboard, parse_mode="Markdown")
+
 
             elif call.data == 'choose_man_more':
                 db = mysql.connector.connect(
@@ -205,7 +204,7 @@ while True:
                 cursor.execute(sql, val)
                 db.commit()
 
-                Send_message.send_message_2(fio[1], MQuery['query_id'])  # отправка исполнителю
+                #Send_message.send_message_2(fio[1], MQuery['query_id'])  # отправка исполнителю
 
                 keyboard = telebot.types.InlineKeyboardMarkup()
                 key_1 = telebot.types.InlineKeyboardButton("Да", callback_data='choose_more')
@@ -314,10 +313,37 @@ while True:
                     pass
 
             elif call.data == 'sent':
-                bot.send_message(call.message.chat.id, 'Сотрудники назначены')
+                db = mysql.connector.connect(
+                    host='localhost',
+                    user='root',
+                    passwd='12345',
+                    port='3306',
+                    database='ogm2'
+                )
+                cursor = db.cursor()
+                sql = "SELECT * FROM employees"
+                cursor.execute(sql, )
+                emps = cursor.fetchall()
+                n = 0
+                for emp in emps:
+                    n += 1
+                    bot.delete_message(call.message.chat.id, call.message.message_id - n)
+                bot.delete_message(call.message.chat.id, call.message.message_id)
+                sql = "SELECT json_emp FROM queries WHERE query_id = %s"  # получение json
+                val = (MQuery['query_id'],)
+                cursor.execute(sql, val)
+                json_emp = cursor.fetchone()[0]
+                json_emp_dict = json.loads(json_emp)
+                doers = json_emp_dict['doers']
+                doers_string = ''
+                for doer in doers:
+                    cursor.execute('SELECT fio FROM employees WHERE employee_id = %s', [doer])
+                    fio = cursor.fetchone()[0]
+                    doers_string = doers_string + ' ' + fio
+                bot.send_message(call.message.chat.id, 'Сотрудники назначены: ' + doers_string)
 
 
-        @bot.message_handler(commands=['menu'])
+        @bot.message_handler(commands=['menu', 'task'])
         def handle_commands(message):
             if message.text == '/menu':
                 emp_id = str(message.chat.id)
@@ -330,6 +356,27 @@ while True:
                     key_2 = telebot.types.InlineKeyboardButton('Отложенные заявки', callback_data='postpone_queries')
                     keyboard.add(key_2)
                     bot.send_message(message.chat.id, 'Меню', reply_markup=keyboard)
+
+            if message.text == '/task':
+                emp_id = str(message.chat.id)
+                if emp_id not in allowed_ids:
+                    bot.send_message(message.chat.id, 'Вам не разрешено пользоваться этим ботом')
+                else:
+                    msg = bot.send_message(message.chat.id, 'Напишите задачи на день...')
+                    bot.register_next_step_handler(msg, task)
+
+        def task(message):
+            tasks = message.text
+            tasks_list = tasks.split('\n\n')
+           #del tasks_list[-1]
+           #del tasks_list[0]
+            for task in tasks_list:
+                if ('адачи' in task) or ('адача' in task):
+                    pass
+                else:
+                    cursor.execute('INSERT INTO daily_tasks (date, status, task) VALUES (%s, %s, %s)', [datetime.now().date(), 'Новая', task])
+                    db.commit()
+                    print(task)
 
         while True:
             try:
